4981a0a1c1854a3e1e5a4f2cdf5a7f2692da83e0
Sango <60422984+MatyilaSango@users.noreply.github.com>
2024-01-24 11:58:57 +0200

    chore: update url

--- Scapping/src/Addon/RootPage/RootPage.ts
+++ Scapping/src/Addon/RootPage/RootPage.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default async function getRootHTMLPage(search: string) {
-  return await fetch(`https://www.accuweather.com/en/search-locations?query=${search}`)
+export default async function getRootHTMLPage(city: string, geo: {lat: string, long: string}) {
+  return await fetch(`https://www.accuweather.com/web-api/three-day-redirect?key=GEO_${geo.long}%2c${geo.lat}&city=${city}&postalCode=&target=`)
     .then((prom) => prom.text());
 }
