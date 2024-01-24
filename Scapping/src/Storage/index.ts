65d3d567c4d367cfa248ff7602f9374de72f0efb
Sango <60422984+MatyilaSango@users.noreply.github.com>
2024-01-24 11:58:32 +0200

    chore update storage from array to map

--- Scapping/src/Storage/index.ts
+++ Scapping/src/Storage/index.ts
@@ -2,67 +2,62 @@ import {
   todayDataType,
   hourlyDataType,
   dailyDataType,
+  // monthlyWeatherData,
 } from "../../../Types/types";
 
-let todayData: todayDataType[] = [];
-let hourlyData: hourlyDataType[] = [];
-let dailyData: dailyDataType[] = [];
+const todayDataMap = new Map<string, todayDataType>()
+const hourlyDataMap = new Map<string, hourlyDataType>()
+const dailyDataMap = new Map<string, dailyDataType>()
+// const monthlyDataMap = new Map<string, monthlyWeatherData>()
 
 //today getter and setter
 export const setToday = (today: todayDataType): void => {
-  todayData.push(today);
+  todayDataMap.set(today.search_parameter, today)
 };
 
 export const getToday = (location: string): todayDataType => {
-  return todayData.filter((today_) =>
-    today_.search_parameter.includes(location)
-  )[0];
+  return todayDataMap.get(location) as todayDataType
 };
 
 export const deleteToday = (location: string): void => {
-  todayData = todayData.filter((today_) => !today_.search_parameter.includes(location));
+  todayDataMap.delete(location);
 };
 
 //hourly getter and setter
 export const setHourly = (hourly: hourlyDataType): void => {
-  hourlyData.push(hourly);
+  hourlyDataMap.set(hourly.search_parameter, hourly);
 };
 
 export const getHourly = (location: string): hourlyDataType => {
-  return hourlyData.filter((hourly_) =>
-    hourly_.search_parameter.includes(location)
-  )[0];
+  return hourlyDataMap.get(location) as hourlyDataType
 };
 
 export const deleteHourly = (location: string): void => {
-  hourlyData = hourlyData.filter((hourly_) => !hourly_.search_parameter.includes(location));
+  hourlyDataMap.delete(location)
 };
 
 //daily getter and setter
-export const setDaily = (daily_: dailyDataType): void => {
-  dailyData.push(daily_);
-};
-
-const formatDateNow = (day: string): String => {
-  let correct_day: Number =
-    Number(day) === 0 || Number(day) === 1 ? 0 : Number(day);
-  let date: Date = new Date();
-  let date_now: string = `${date.getMonth() + 1}/${
-    date.getDate() + Number(correct_day)
-  }`;
-  return date_now;
+export const setDaily = (daily: dailyDataType, day: string): void => {
+  dailyDataMap.set(`${daily.search_parameter}_${day}`, daily);
 };
 
 export const getDaily = (location: string, day: string): dailyDataType => {
-  return dailyData.filter((daily_) =>
-    daily_.search_parameter.includes(location)
-  )[0];
+  return dailyDataMap.get(`${location}_${day}`) as dailyDataType;
 };
 
 export const deleteDaily = (location: string, day: string): void => {
-  let date_now: String = formatDateNow(day);
-  dailyData = dailyData.filter(
-    (daily_) =>
-      !(daily_.search_parameter.includes(location) && daily_.date === date_now)
-  );
+  dailyDataMap.delete(`${location}_${day}`)
 };
+
+// //monthly getter and setter
+// export const setMonthly = (location: string, monthly: monthlyWeatherData): void => {
+//   monthlyDataMap.set(`${location}_${monthly.month}`, monthly);
+// };
+
+// export const getMonthly  = (location: string, month: string): monthlyWeatherData => {
+//   return monthlyDataMap.get(`${location}_${month}`) as monthlyWeatherData;
+// };
+
+// export const deleteMonthly  = (location: string, month: string): void => {
+//   monthlyDataMap.delete(`${location}_${month}`)
+// };
